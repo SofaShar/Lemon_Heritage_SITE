361ce99411aba41ad64af92e65d1b7bc9c513723
SofaShar <sharova_sonya@inbox.ru>
2026-07-05 19:38:17 +0300

    Update vite.config.mjs

--- site/vite.config.mjs
+++ site/vite.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+  plugins: [react()],
+  base: '/Lemon_Heritage_SITE/',
 export default defineConfig({
   build: {
     outDir: "build",
   },
-  plugins: [react()],
-  base: '/Lemon_Heritage_SITE/',
 });
